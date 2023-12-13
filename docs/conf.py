bc1ae2c8ae8ffc50d532332a2b3ac25c94bb1568
sjbrandenberg <sjbrandenberg@ucla.edu>
2023-12-13 09:37:52 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,12 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'pydata_sphinx_theme'
-
+html_theme_options = {
+    "logo": {
+        "text": "ucla_geotech_tools",
+    }
+}
+    
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
